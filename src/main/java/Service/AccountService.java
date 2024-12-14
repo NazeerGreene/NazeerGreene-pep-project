ebeb48ee05b333b18e73eec385fb4a44e0732edd
Nazeer Greene <greene.nazeer@gmail.com>
2024-12-14 03:28:00 +0000

    finished account service layer

--- src/main/java/Service/AccountService.java
+++ src/main/java/Service/AccountService.java
@@ -3,33 +3,65 @@ package Service;
 import DAO.AccountDao;
 import Model.Account;
 
+/*
+ * Service rules
+ *  New & Returning Users:
+ *  1. username not blank
+ *  2. password 4+ characters long
+ *  
+ *  New Users:
+ *  3. username not present in database
+*/
 public class AccountService {
     private final AccountDao accountDao;
+
+    private final int MIN_PASSWORD_LENGTH = 4;
 
     public AccountService() {
         accountDao = new AccountDao();
     }
 
+    /*
+     * @param Account with username and password to register new user
+     * @return Account (id, username, password) if service rules met
+    */
     public Account registerNewUser(Account account) {
-        return null;
-        // rules
-        // username not blank
-        // password 4+ char long
-        // username not already taken
-        // then create new account
-        // else reject
+
+        // business rules
+        if(!credentialsAreValid(account.getUsername(), account.getPassword())) {
+            return null;
+        }
+
+        if (accountDao.usernameTaken(account.getUsername())) {
+            return null;
+        }
+
+        return accountDao.registerNewUser(account);
     }
 
+    /*
+     * @param Account with username and password to verify
+     * @return Account (id, username, password) if found in database
+    */
     public Account verifyExistingUser(Account account) {
-        return null;
-        // rules
-        // username and password match cred in db
-        // return account cred
-        // else reject
+        // business rules - redudant, but could save a trip to the database
+        if (!credentialsAreValid(account.getUsername(), account.getPassword())) {
+            return null;
+        }
+
+        return accountDao.verifyExistingUser(account);
     }
 
-    private boolean verifyCredentialsRules(String username, String password) {
-        return false;
+    private boolean credentialsAreValid(String username, String password) {
+        if (username.isBlank()) {
+            return false;
+        }
+
+        if (password.length() < MIN_PASSWORD_LENGTH) {
+            return false;
+        } 
+
+        return true;
     }
     
 }
